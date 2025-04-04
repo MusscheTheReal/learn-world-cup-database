0641846f90998a416b4ae6e0cd98b762592765d5
Philippe Vinckier <philippe.vinckier1995@gmail.com>
2025-04-04 12:18:03 +0000

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,33 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNERGOALS OPPONENTGOALS
+do
+  if [[ $WINNER != 'winner' ]]
+  then
+    # get team ID for the WINNER
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER';")
+    # if winner_id is empty
+    if [[ -z $WINNER_ID ]]
+    then
+      # add winner to teams
+      echo $($PSQL "INSERT INTO teams(name) VALUES ('$WINNER');")
+      # get new winner_id
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER';")
+    fi
+
+    # get team ID for the OPPONENT
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT';")
+    if [[ -z $OPPONENT_ID ]]
+    then
+      # add opponent to teams
+      echo $($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT');")
+      # get new ID
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT';")
+    fi
+
+    # add to games
+    echo $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNERGOALS, $OPPONENTGOALS);")
+  fi
+done
